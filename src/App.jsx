73f53ff079a423dcaacc7f0158f19a2e51b8ad76
kennzuki <kingsyken@gmail.com>
2024-11-01 02:34:41 +0300

    weather data ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { CiSun } from "react-icons/ci"
+
 function App() {
   return (
     <div className='py-12 px-14 flex flex-col gap-8 bg-[#D5E7FE]'>
@@ -7,6 +9,14 @@ function App() {
       {/* search city */}
       <section className="">
         <input type="text" className="rounded-xl w-full shadow-lg px-2 py-4" placeholder="Search for city..." />
+      </section>
+      {/* weather data */}
+      <section className="mt-12 bg-gray-100 p-4 rounded-xl">
+        <section className="flex text-3xl font-bold justify-between place-items-center">
+          <h1 className="flex flex-col gap-2 text-start text-6xl"><span className="">87°F</span> New York</h1>
+          <CiSun size={45} />
+        </section>
+        <p className="font-semibold mt-4">Partly Cloudy</p>
       </section>
     </div>
   );
