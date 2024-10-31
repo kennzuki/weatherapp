460039c536cca9c313ffc34a8e4889215dba8b10
kennzuki <kingsyken@gmail.com>
2024-11-01 01:51:58 +0300

    navbar and heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <div className='py-12 px-14 flex flex-col gap-8 bg-[#D5E7FE]'>
+      <h1 className='text-3xl text-center text-blue-500 font-bold underline'>
+        Weather App
+      </h1>
+      {/* search city */}
+      <section className=""></section>
+    </div>
   );
 }
 
